0b0ec25fe503a1ea16a4b5d5cac5cf5775b07d58
Juan <jb@hotmail.com>
2021-06-05 14:07:21 -0300

    agrego errores en db.js

--- src/database.js
+++ src/database.js
@@ -3,6 +3,72 @@ const { DB_CONFIG } = require('./config');
 
 let connection;
 
+/**
+ * Error específico para recursos inexistentes en la DB.
+ */
+ class ResourceNotFoundError extends Error {
+    /**
+     * @param {string} message
+     * @param {string} resource
+     * @param {number} id
+     */
+    constructor(message, resource, id) {
+      super(message);
+      this.resource = resource;
+      this.id = id;
+    }
+  }
+  
+  /**
+   * @typedef {Object} TUser
+   * @property {string} username Usuario.
+   * @property {string} password Password.
+   * @property {string} name Nombre.
+   * @property {number} age Edad.
+   *
+   * @typedef {TUser & {
+   *    id: number,
+   *  }} TUserDB
+   *
+   * @typedef {Object} TFilterQuery
+   * @property {string|undefined} username Usuario.
+   * @property {string|undefined} password Password.
+   * @property {string|undefined} name Nombre.
+   * @property {number|undefined} age Edad.
+   */
+  
+  /**
+   * Validar datos del usuario.
+   *
+   * @param {TUser} userData
+   */
+  function validateUser(userData) {
+    if (!userData) {
+      throw new Error(`No se ha definidio el usuario`);
+    }
+  
+    if (!userData.name || !userData.name.trim()) {
+      throw new Error(`La propiedad 'name' es requerida`);
+    }
+  
+    if (userData.username && !userData.username.trim()) {
+      throw new Error(`La propiedad 'username' no puede ser vacía`);
+    }
+  
+    if (userData.password && !userData.password.trim()) {
+      throw new Error(`La propiedad 'password' no puede ser vacía`);
+    }
+  
+    if (
+      !userData.age ||
+      isNaN(userData.age) ||
+      userData.age < 18 ||
+      userData.age > 200
+    ) {
+      throw new Error(`La propiedad 'age' es inválida`);
+    }
+  };
+
 
 module.exports = {
     async initDB() {
