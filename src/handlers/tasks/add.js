710d8a9945abde7ba985297ff2f82b67ca0e86e4
Juan <jb@hotmail.com>
2021-06-05 19:31:01 -0300

    agrego endpoint "add" funcionando

--- src/handlers/tasks/add.js
+++ src/handlers/tasks/add.js
@@ -1,5 +1,29 @@
+// module.exports = (route) => {
+//     route.get('/',(req, res) => {
+//         res.json("hola");
+//       })
+//   };
+
+const database = require('../../database');
+const validateErrors = require('../../validations/validateErrors');
+
+
 module.exports = (route) => {
-    route.get('/',(req, res) => {
-        res.json("hola");
-      })
-  };
+  route.post(
+    '/',
+    
+    validateErrors,
+    (async (req, res) => {
+      const title = req.body.title;
+      const description = req.body.description;
+
+
+      const newTask = await database.add({
+        title,
+        description,
+      });
+
+      res.json(newTask);
+    })
+  );
+};
