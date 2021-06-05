3f08c7279911c1366dd8676bd32aa32dbad7a59c
Juan <jb@hotmail.com>
2021-06-05 13:48:07 -0300

    agrego routing inicial

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { PORT } = require('./src/config');
+const routingAPI = require('./src/handlers');
 const { initDB } = require('./src/database');
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routingAPI);
 app.use(express.static('./public'));
 
 
---
+++ src/handlers/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const apiRouting = express.Router();
+
+
+module.exports = apiRouting;
