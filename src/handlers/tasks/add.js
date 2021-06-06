f3e2c866af61187dcb2d87cffc1a4da9a43249a5
Juan <jb@hotmail.com>
2021-06-06 11:38:17 -0300

    agrego requestHandler

--- src/handlers/tasks/add.js
+++ src/handlers/tasks/add.js
@@ -1,13 +1,13 @@
 const database = require('../../database');
 const validateErrors = require('../../validations/validateErrors');
-
+const requestHandler = require('../../middlewares/requestHandler');
 
 module.exports = (route) => {
   route.post(
     '/',
     
     validateErrors,
-    (async (req, res) => {
+    requestHandler(async (req, res) => {
       const title = req.body.title;
       const description = req.body.description;
 
